b80d0103cfda1f54f3d2869c96b0b0dd8a82a9c2
Moshe Kabala <31233967+moshe-kabala@users.noreply.github.com>
2020-09-03 11:53:00 +0300

    Update trainer_mpi.go

--- cmd/arena/commands/trainer_mpi.go
+++ cmd/arena/commands/trainer_mpi.go
@@ -102,13 +102,6 @@ func (mj *MPIJob) GetStatus() string {
 	if mj.mpijob.Status.ReplicaStatuses == nil {
 		return "Pending"
 	}
-	if mj.mpijob.Status.ReplicaStatuses != nil {
-		if mj.mpijob.Status.ReplicaStatuses["Launcher"].Succeeded == 1 {
-			jobStatus = constants.Status.Succeeded
-		} else if mj.mpijob.Status.ReplicaStatuses["Launcher"].Failed == 1 {
-			jobStatus = constants.Status.Failed
-		}
-	}
 	return getTrainingStatus(mj.mpijob.Annotations, &mj.chiefPod, jobStatus)
 }
 
