58c6a2f695861fc4eecb5387ebff24fd61280d29
Moshe Kabala <moshek@MacBook-Pro-3.local>
2020-09-22 16:10:07 +0300

    return bake the message try to connect ...

--- cmd/arena/commands/attach.go
+++ cmd/arena/commands/attach.go
@@ -84,6 +84,11 @@ func Attach(cmd *cobra.Command, jobName string, stdin, tty bool, podName string,
 		return fmt.Errorf("Not found any matching pod")
 	}
 
+	if podName == "" {
+		// notify the user which pod name he will to attach
+		fmt.Println("Trying to connect to a pod called:", podToExec.Name)
+	}
+
 	return attachByKubeCtlBin(podToExec, stdin, tty)
 }
 
