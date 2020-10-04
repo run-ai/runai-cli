e72627f878f5907aa48cb4d712e1417b58464158
yodarshafrir1 <yodarshafrir@gmail.com>
2020-10-04 18:27:47 +0300

    Update fractional and HPO error take 2

--- cmd/submit_runai.go
+++ cmd/submit_runai.go
@@ -277,7 +277,7 @@ func verifyHPOFlags(submitArgs *submitRunaiJobArgs) error {
 			return fmt.Errorf("interactive jobs can't run with Parallelism")
 		}
 		if *submitArgs.GPU != 0 && *submitArgs.GPU < 1 {
-			return fmt.Errorf("usage of GPU Fractions in conjunction with parallelism is not supported")
+			return fmt.Errorf("usage of GPU Fractions in conjunction together with parallelism is not supported")
 		}
 	}
 	return nil
