9382601bccca236542ccdb36d851ea1e239b415c
Moshe <moshe@run.ai>
2020-10-08 01:20:06 +0300

    CR-FIXES-5

--- pkg/util/cache.go
+++ pkg/util/cache.go
@@ -15,6 +15,21 @@ const (
 	EmptyCacheStatus 	CacheStatus = "Empty"
 )
 
+// Cache design to be able to save async data in lazy mode (request the data only if someone ask it) and provide it to any goroutine thread 
+// for example, we have the following function 
+// 
+//    func GetSomeData(id string) Data {
+//       client := clientCache.Get()
+//       return client.GetDataById(id)
+//    }
+// 
+// and in another area, we call it in parallel
+// 
+//    go GetSomeData(id1)
+//    go GetSomeData(id2)
+//    go GetSomeData(id3)
+// 
+// the cache will keep that the function "clientCache.Get()" will be call to the data only once
 type Cache interface  {
 	Refresh() (interface{}, error)
 	Get() (interface{}, error)
@@ -28,7 +43,7 @@ type cacheImpl struct {
 	status CacheStatus
 	waitChan chan GetCacheDataFunc
 	waitersCounter int
-	mux sync.Mutex
+	mutex sync.Mutex
 }
 
 func NewCache(get GetCacheDataFunc) Cache {
@@ -45,21 +60,22 @@ func (c *cacheImpl) wait() (interface{}, error) {
 }
 
 func (c *cacheImpl) Get() (interface{}, error) {
-	c.mux.Lock()
+	// need to ensure that only one thread check the status to prevent more than one to call c.Refresh
+	c.mutex.Lock()
 	switch c.status {
 	case EmptyCacheStatus:
 		c.status = FetchingCacheStatus
-		c.mux.Unlock()
+		c.mutex.Unlock()
 		return c.Refresh()
 	case FetchingCacheStatus:
 		c.waitersCounter++
-		c.mux.Unlock()
+		c.mutex.Unlock()
 		return c.wait()
 	case ReadyCacheStatus:
-		c.mux.Unlock()
+		c.mutex.Unlock()
 		return c.data, c.err
 	default: 
-		c.mux.Unlock()
+		c.mutex.Unlock()
 		return nil, fmt.Errorf("[Cache] Unknown cache status: %s", c.status)
 	}
 }
@@ -70,11 +86,11 @@ func (c *cacheImpl) Status() CacheStatus {
 
 
 func (c *cacheImpl) Refresh() (interface{}, error) {
-	c.mux.Lock()
+	c.mutex.Lock()
 	c.status = FetchingCacheStatus
-	c.mux.Unlock()
+	c.mutex.Unlock()
 	data, err := c.get_data()
-	c.mux.Lock()
+	c.mutex.Lock()
 	c.data = data
 	c.err = err
 	c.status = ReadyCacheStatus
@@ -82,6 +98,6 @@ func (c *cacheImpl) Refresh() (interface{}, error) {
 		c.waitChan <- (func() (interface{}, error) { return data, err })
 	}
 	c.waitersCounter = 0
-	c.mux.Unlock()
+	c.mutex.Unlock()
 	return data, err
 }
