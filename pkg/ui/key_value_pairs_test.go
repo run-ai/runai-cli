3a34a0c5a3bbf27df8ddf07f25b4f9a1ce339d8d
Moshe <moshe@run.ai>
2020-10-15 15:40:36 +0300

    ordering the test

--- pkg/ui/key_value_pairs_test.go
+++ pkg/ui/key_value_pairs_test.go
@@ -2,7 +2,6 @@ package ui
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -21,12 +20,12 @@ func TestKeyValuePairs(t *testing.T) {
 	t.Run("General Case", func(t *testing.T) {
 
 		b := new(bytes.Buffer)
-		expected := from_file("expecteds/key_value_pairs_test_1.txt")
-
+		expactedPath := "test_expected/key_value_pairs_test_1.txt"
+		expected := from_file(expactedPath)
 		w := tabwriter.NewWriter(b, 0, 0, 2, ' ', 0)
 		addr := Address{y: float32ptr(552.38956564), x: 3}
 		data := Person{"moshe", 30, addr, &addr}
-		// {"", 50, addr, &addr},
+		
 		err := CreateKeyValuePairs(Person{}, KeyValuePairsOpt{}).Render(w, data).Error()
 		if err == nil {
 			_ = w.Flush()
@@ -36,9 +35,10 @@ func TestKeyValuePairs(t *testing.T) {
 
 		got := b.String()
 
+		// for test debugging
+		// record_at_file(expactedPath, got)
 
 		if got != expected {
-			fmt.Println(got)
 			t.Errorf("Strings dont match expected:\n\n%s\nresult: \n\n%s", expected, got)
 		}
 
@@ -55,7 +55,7 @@ func from_file(fileName string) string {
 
 // util to save the result at a file
 func record_at_file(fileName string, content string) {
-	f, err := os.Create("textoutput.txt")
+	f, err := os.Create(fileName)
 	defer f.Close()
 	check(err)
 	f.WriteString(content)
--- pkg/ui/table_test.go
+++ pkg/ui/table_test.go
@@ -14,15 +14,15 @@ type Address struct {
 type Person struct {
 	name       string `title:"Name" def:"--"`
 	Age        int
-	address    Address  `group:"Addres"`
-	addressPtr *Address `group:"Addres2"`
+	address    Address  `group:"Address"`
+	addressPtr *Address `group:"Address 2"`
 }
 
 func TestTable(t *testing.T) {
 	t.Run("General Case", func(t *testing.T) {
-
+		expactedPath := "test_expected/table_test_1.txt"
+		expected := from_file(expactedPath)
 		b := new(bytes.Buffer)
-		expected := from_file("expecteds/table_test_1.txt")
 		w := tabwriter.NewWriter(b, 0, 0, 2, ' ', 0)
 		addr := Address{y: float32ptr(552.38956564), x: 3}
 		data := []Person{
@@ -38,6 +38,9 @@ func TestTable(t *testing.T) {
 		}
 
 		got := b.String()
+
+		// for test debugging
+		// record_at_file(expactedPath, got)
 
 		if got != expected {
 			t.Errorf("Strings dont match expected:\n\n%s\n result: \n\n%s", expected, got)
