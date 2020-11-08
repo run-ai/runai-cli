e8fc3b235f0e91d314768c8087dca62f43088823
omer <omer@run.ai>
2020-11-08 14:35:00 +0200

    Add ability to have multiple env variables

--- cmd/job/submit/submit_template_util_test.go
+++ cmd/job/submit/submit_template_util_test.go
@@ -5,25 +5,6 @@ import (
 	"testing"
 )
 
-func TestMergeEnvironmentVariablesNoConflict(t *testing.T) {
-	cliEnvVar := []string{ "user=test-user" }
-	templateEnvVar := []string{ "server=127.0.0.1" }
-
-	mergeResult := mergeEnvironmentVariables(&cliEnvVar, &templateEnvVar)
-
-	assert.Equal(t, len(mergeResult), 2)
-}
-
-func TestMergeEnvironmentVariablesOverride(t *testing.T) {
-	cliEnvVar := []string{ "user=test-user" }
-	templateEnvVar := []string{ "user=override" }
-
-	mergeResult := mergeEnvironmentVariables(&cliEnvVar, &templateEnvVar)
-
-	assert.Equal(t, len(mergeResult), 1)
-	assert.Equal(t, mergeResult[0], "user=test-user")
-}
-
 func TestMergeBoolFlags(t *testing.T) {
 	cliBoolFlag := false
 	templateBoolFlag := true
--- pkg/templates/submit_template_merger_test.go
+++ pkg/templates/submit_template_merger_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	truePtr = true
+	truePtr  = true
 	falsePtr = false
 )
 
@@ -46,7 +46,6 @@ func TestMergeTemplateFieldsUnOverrideNil(t *testing.T) {
 	assert.Equal(t, *merged.Required, true)
 }
 
-
 func TestMergeSubmitTemplatesSanity(t *testing.T) {
 	baseSubmitTemplate := SubmitTemplate{Gpu: &TemplateField{Required: &falsePtr}, Name: &TemplateField{Value: "Tester"}, EnvVariables: []string{"a"}}
 	patchSubmitTemplate := SubmitTemplate{Gpu: &TemplateField{Required: &truePtr}, Image: &TemplateField{Value: "MyTestImage"}, EnvVariables: []string{"b"}}
@@ -57,4 +56,23 @@ func TestMergeSubmitTemplatesSanity(t *testing.T) {
 	assert.Equal(t, result.Name.Value, "Tester")
 	assert.Equal(t, result.Image.Value, "MyTestImage")
 	assert.Equal(t, len(result.EnvVariables), 2)
+}
+
+func TestMergeEnvironmentVariablesNoConflict(t *testing.T) {
+	cliEnvVar := []string{"user=test-user"}
+	templateEnvVar := []string{"server=127.0.0.1"}
+
+	mergeResult := MergeEnvironmentVariables(&cliEnvVar, &templateEnvVar)
+
+	assert.Equal(t, len(mergeResult), 2)
+}
+
+func TestMergeEnvironmentVariablesOverride(t *testing.T) {
+	cliEnvVar := []string{"user=test-user"}
+	templateEnvVar := []string{"user=override"}
+
+	mergeResult := MergeEnvironmentVariables(&cliEnvVar, &templateEnvVar)
+
+	assert.Equal(t, len(mergeResult), 1)
+	assert.Equal(t, mergeResult[0], "user=test-user")
 }
