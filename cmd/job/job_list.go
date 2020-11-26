c673b6a7b0445ec0bfca08391cb35f6388b71ce8
omer <omer@run.ai>
2020-11-26 16:28:07 +0200

    A

--- cmd/job/job_list.go
+++ cmd/job/job_list.go
@@ -36,7 +36,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const jobInvalidStateOnCreationTimeInSeconds = 10
+const jobInvalidStateOnCreationTimeInSeconds = 30
 
 func ListCommand() *cobra.Command {
 	var allNamespaces bool
