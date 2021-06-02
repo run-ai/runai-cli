885943f0810e2c900354ea573b73dbd904c8a141
Erez Freiberger <enoodle@gmail.com>
2021-06-02 16:21:31 +0300

    Allow installing runai on any path

--- install-runai.sh
+++ install-runai.sh
@@ -10,16 +10,6 @@ NEW_SCRIPT_FILES=${1:-/usr/local/runai}
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")"; pwd)"
 
-# Remove old version files
-if [ -d "${OLD_SCRIPT_FILES}" ]; then
-  rm -rf "${OLD_SCRIPT_FILES}"
-fi
-
-# Remove new version files
-if [ -d "${NEW_SCRIPT_FILES}" ]; then
-  rm -rf "${NEW_SCRIPT_FILES}"
-fi
-
 # Create copy destination if it doesn't exist to have directories copied under the folder.
 if [ ! -d "${NEW_SCRIPT_FILES}" ]; then
   mkdir "${NEW_SCRIPT_FILES}"
@@ -29,4 +19,8 @@ cp "${SCRIPT_DIR}"/runai "${NEW_SCRIPT_FILES}"
 cp "${SCRIPT_DIR}"/VERSION "${NEW_SCRIPT_FILES}"
 cp -R "${SCRIPT_DIR}"/charts "${NEW_SCRIPT_FILES}"
 
-ln -sf "${NEW_SCRIPT_FILES}"/"${SCRIPT_NAME}" /usr/local/bin/"${SCRIPT_NAME}"
+if [ "$NEW_SCRIPT_FILES" == "/usr/local/runai" ] ; then
+    ln -sf "${NEW_SCRIPT_FILES}"/"${SCRIPT_NAME}" /usr/local/bin/"${SCRIPT_NAME}"
+else
+    echo "Add ${NEW_SCRIPT_FILES} to your \$PATH"
+fi
