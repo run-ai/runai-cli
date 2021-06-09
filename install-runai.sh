aa61e4dd668701329d06dfc96267e8cb00a35f50
Erez Freiberger <enoodle@gmail.com>
2021-06-09 13:33:21 +0300

    fixing cli updating + update on any path

--- install-runai.sh
+++ install-runai.sh
@@ -5,28 +5,33 @@ SCRIPT_NAME=runai
 
 # If first argument is not empty,
 # use that for the installation path
-NEW_SCRIPT_FILES=${1:-/usr/local/runai}
+NEW_SCRIPT_PATH=${1:-/usr/local}
 
 SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")"; pwd)"
 
-# Create copy destination if it doesn't exist to have directories copied under the folder.
-if [ ! -d "${NEW_SCRIPT_FILES}" ]; then
-    if [ "$NEW_SCRIPT_FILES" == "/usr/local/runai" ]; then
-        mkdir "${NEW_SCRIPT_FILES}"
-    else
-        echo "${NEW_SCRIPT_FILES} doesn't exist or is not a directory"
-        ls "${NEW_SCRIPT_FILES}" 2> /dev/null
-    fi
+# Remove old version files
+if [ -d "${NEW_SCRIPT_PATH}/runai" ]; then
+  rm -rf "${NEW_SCRIPT_PATH}/runai"
 fi
 
-cp "${SCRIPT_DIR}"/runai "${NEW_SCRIPT_FILES}"
-cp "${SCRIPT_DIR}"/VERSION "${NEW_SCRIPT_FILES}"
-cp -R "${SCRIPT_DIR}"/charts "${NEW_SCRIPT_FILES}"
+# Create copy destination if it doesn't exist to have directories copied under the folder.
+if [ ! -d "${NEW_SCRIPT_PATH}" ]; then
+    echo "${NEW_SCRIPT_PATH} doesn't exist or is not a directory"
+    ls "${NEW_SCRIPT_PATH}" 2> /dev/null
+fi
+if [ ! -d "${NEW_SCRIPT_PATH}/runai" ]; then
+    mkdir "${NEW_SCRIPT_PATH}/runai"
+fi
+NEW_SCRIPT_PATH="${NEW_SCRIPT_PATH}/runai"
 
-if [ "$NEW_SCRIPT_FILES" == "/usr/local/runai" ] ; then
-    ln -sf "${NEW_SCRIPT_FILES}"/"${SCRIPT_NAME}" /usr/local/bin/"${SCRIPT_NAME}"
+cp "${SCRIPT_DIR}"/runai "${NEW_SCRIPT_PATH}"
+cp "${SCRIPT_DIR}"/VERSION "${NEW_SCRIPT_PATH}"
+cp -R "${SCRIPT_DIR}"/charts "${NEW_SCRIPT_PATH}"
+
+if [ "$NEW_SCRIPT_PATH" == "/usr/local/runai" ] ; then
+    ln -sf "${NEW_SCRIPT_PATH}"/"${SCRIPT_NAME}" /usr/local/bin/"${SCRIPT_NAME}"
 else
-    echo "Add ${NEW_SCRIPT_FILES} to your \$PATH: export PATH=\$PATH:${NEW_SCRIPT_FILES}"
+    echo "Add ${NEW_SCRIPT_PATH} to your \$PATH: export PATH=\$PATH:${NEW_SCRIPT_PATH}"
 fi
 
 echo "Run:AI CLI installed successfully!"
