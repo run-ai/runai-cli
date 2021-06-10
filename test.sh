ccd59268f78a7b9647070f7a91197f651a888cbf
itayvallach <itay@run.ai>
2021-06-10 12:06:58 +0300

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
+git config --global url."https://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:x-oauth-basic@github.com/run-ai".insteadOf "https://github.com/run-ai" || true
 GO111MODULE=on ${GENERAL_BUILD_OPTIONS} go test ./... -v -tags test
+
