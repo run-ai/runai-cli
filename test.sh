5333341767d006049e1b4c9a2bacefff76cc0144
itayvallach <itay@run.ai>
2021-06-10 12:12:02 +0300

    wip

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-git config --global url."https://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:x-oauth-basic@github.com/run-ai".insteadOf "https://github.com/run-ai" || true
+git config --global url."https://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:x-oauth-basic@github.com/run-ai".insteadOf "https://github.com/run-ai" || true
+
 GO111MODULE=on ${GENERAL_BUILD_OPTIONS} go test ./... -v -tags test
 
